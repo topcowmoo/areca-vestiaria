b472ae439f3f8dfb774aaa1aafc1a361f14d7f08
garrethil <garrethildebrandt@gmail.com>
2024-07-25 15:46:55 -0400

    setup skeleton of landing Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 function Home() {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-3xl font-bold text-gray-800">Home Coming Soon</h1>
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <div className="flex justify-center py-4">
+        <h2>**Nav Bar Here**</h2>
       </div>
-    );
-  }
-  
-  export default Home;
-  
+
+      <div className="flex flex-grow flex-col items-center justify-center">
+        <h1 className="text-xl">WLNS</h1>
+        <h4 className="text-s">The Wellness Hub</h4>
+        <h4>**Search Bar Here**</h4>
+        <a href="" className="text-xs">
+          Explore More
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
