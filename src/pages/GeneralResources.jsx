e335b44d2e25d6e684533fc0be785aa3c05f49e6
smbanati-IZxCKm <smbanati@gmail.com>
2024-07-27 03:39:46 -0400

    added additional pages mentioned in standup and ticket

---
+++ src/pages/GeneralResources.jsx
@@ -0,0 +1,11 @@
+
+
+function GeneralResources() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <h1 className="text-3xl font-bold text-gray-800">General Resources Coming Soon!</h1>
+  </div>
+  )
+}
+
+export default GeneralResources
---
+++ src/pages/Therapy.jsx
@@ -0,0 +1,11 @@
+
+
+function Therapy() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <h1 className="text-3xl font-bold text-gray-800">Therapy Coming Soon</h1>
+  </div>
+  )
+}
+
+export default Therapy
