b3c2f91e5fc234b202fc7a469158b7b6f0f4c146
Salvatore <mammoliti.sam@gmail.com>
2024-07-28 15:54:51 -0400

    reverted return statement in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import ParentingFilterBox from "../components/ParentingFilterBox";
+
 
 function Home() {
+
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <ParentingFilterBox />
-      </div>
-    );
+      <h1 className="text-3xl font-bold text-gray-800">Homepage Coming Soon!</h1>
+    </div>
+    )
   }
   
   export default Home;
