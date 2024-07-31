d1037c8a7cff5ad180e22a9b2daa4952105125d3
garrethil <garrethildebrandt@gmail.com>
2024-07-31 11:21:27 -0400

    style secont page of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,8 @@ function Home() {
   };
 
   return (
+    //Top page
+
     <div>
       <div className="flex flex-col min-h-screen bg-page-one bg-cover bg-center">
         <div className="flex justify-center">
@@ -40,30 +42,29 @@ function Home() {
       </div>
 
       <div className="flex flex-col min-h-screen bg-page-two bg-cover bg-center p-6">
-        <div className="flex flex-grow flex-col items-center justify-center">
-          <h1 className="text-6xl mb-4">
-            Sunrise Health & Clinical <br /> Excellence Centre
-          </h1>
-          <div className="w-3/4 px-6">
-            <p className="mb-4 text-lg">
+        <div className="flex flex-grow flex-col items-center justify-center mx-12 my-12">
+          <div className="w-3/4 flex flex-grow flex-col items-start justify-center">
+            <div className="w-[1014px] text-white text-[71px] font-extrabold font-playfair leading-[67.45px] mb-4">
+              Sunrise Health & Clinical Excellence Centre
+            </div>
+            <div className="w-full text-white text-[19px] font-light font-['Inter'] leading-[37px] mt-8">
               Sun rises everyday - and we believe that in mental health and life
-              in general- hope and new beginnings are key ingredients to
+              in general - hope and new beginnings are key ingredients to
               wellness. At SUNRISE, not only do we focus on comprehensive
               psychiatric care - but also wellness as both are essential for
               long term well being. Our team is going to be expanding over time
-              and currently consists of Dr. Mammoliti and Dr. Eletu.
-            </p>
-            <p>
+              and currently consists of Dr. Mammoliti and Dr. Eletu. <br />
+              <br />
               Being a medical doctor, a consultation with Dr. Mammoliti or Dr.
-              Eletu is covered by OHIP as well as ongoing care (your health
-              card) if you have OHIP coverage. THERE IS NO CAPACITY FOR ONGOING
-              PSYCHIATRIC CARE AT THIS TIME WITH EITHER DOCTOR AND CONSULTS ARE
-              AVAILABLE on a limited basis given the demand and expertise
-              fields.
-            </p>
+              Eletu is covered by OHIP as well as ongoing care <br />
+              (your health card) if you have OHIP coverage. THERE IS NO CAPACITY
+              FOR ONGOING PSYCHIATRIC CARE AT THIS TIME WITH EITHER DOCTOR AND
+              CONSULTS ARE AVAILABLE on a limited basis given the demand and
+              expertise fields.{" "}
+            </div>
             <button
               onClick={handleOpenModal}
-              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              className="mt-4 px-4 py-2 bg-white text-white font-inter rounded text-[#161616] hover:text-white hover:bg-[#161616]"
             >
               Read More
             </button>
