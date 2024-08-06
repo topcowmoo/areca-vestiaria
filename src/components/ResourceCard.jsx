a54537ab2ee5399bdbc940e9bffca5520dea9876
garrethil <garrethildebrandt@gmail.com>
2024-08-06 13:51:33 -0400

    add Resource card component to AllResources page

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 const ResourceCard = ({ title, image, link }) => {
   return (
     <div
-      className={`relative w-[360px] h-[331px] rounded-lg overflow-hidden bg-cover bg-center ${image}`}
+      className={`relative w-[450px] h-[408px] rounded-lg overflow-hidden bg-cover bg-center ${image}`}
     >
       <div className="absolute inset-0 flex flex-col justify-center p-8">
-        <h2 className="text-white text-lg font-playfair text-left">{title}</h2>
-        <a href={link} className="text-white text-sm underline text-left py-4">
+        <h2 className="text-white text-[60px] font-playfair text-left">
+          {title}
+        </h2>
+        <a href={link} className="text-white text-lg underline text-left py-4">
           View More
         </a>
       </div>
