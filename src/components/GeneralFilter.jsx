5c9cb3cb5cfccf098fd0f4337f88d770de6029a1
Salvatore <mammoliti.sam@gmail.com>
2024-08-18 12:57:51 -0400

    refactor responsiveness

--- src/components/GeneralFilter.jsx
+++ src/components/GeneralFilter.jsx
@@ -3,7 +3,12 @@ import PropTypes from "prop-types";
 import { collectionData } from "../data/collectionData";
 import { RiEqualizerFill } from "react-icons/ri";
 
-const GeneralFilter = ({ currentPage, setFilteredData, setIsFilterBoxOpen, isFilterBoxOpen }) => {
+const GeneralFilter = ({
+  currentPage,
+  setFilteredData,
+  setIsFilterBoxOpen,
+  isFilterBoxOpen,
+}) => {
   const [checkedKinds, setCheckedKinds] = useState({
     Article: false,
     Video: false,
@@ -11,7 +16,9 @@ const GeneralFilter = ({ currentPage, setFilteredData, setIsFilterBoxOpen, isFil
   });
 
   const filteredData = useMemo(() => {
-    const kinds = Object.keys(checkedKinds).filter((kind) => checkedKinds[kind]);
+    const kinds = Object.keys(checkedKinds).filter(
+      (kind) => checkedKinds[kind]
+    );
 
     if (kinds.length === 0) {
       return collectionData.filter((item) => item.category === currentPage);
@@ -36,55 +43,95 @@ const GeneralFilter = ({ currentPage, setFilteredData, setIsFilterBoxOpen, isFil
     setIsFilterBoxOpen(!isFilterBoxOpen);
   };
 
-  return (
-    <div className="relative">
-      {/* Move the button to the desired location using CSS or layout changes */}
+return (
+    <div className="relative w-full">
+      {/* Button styles for large and small screens */}
       <div
-        className={`h-[33.95px] px-[9.59px] py-[4.47px] rounded-md border justify-start items-center gap-[7.67px] inline-flex cursor-pointer ${
+        className={`h-[33.95px] px-[9.59px] py-[4.47px] justify-start items-center gap-[7.67px] inline-flex cursor-pointer ${
           isFilterBoxOpen
-            ? "bg-white text-black border-white"
-            : "bg-black text-white border-white"
-        }`}
+            ? "lg:bg-white lg:text-black sm:bg-white sm:text-black sm:border-none"
+            : "lg:bg-black lg:text-white sm:bg-black sm:text-white"
+        } ${
+          !isFilterBoxOpen
+            ? "rounded-md border border-white"
+            : "rounded-md border-none"
+        } sm:h-[28px] sm:px-[8px] sm:py-[4px] sm:gap-[5px] lg:h-[33.95px] lg:px-[9.59px] lg:py-[4.47px] lg:gap-[7.67px]`}
         onClick={toggleFilterBox}
       >
         <RiEqualizerFill
-          className="w-[24.29px] h-[24.29px]"
-          style={{ color: isFilterBoxOpen ? "black" : "white" }}
+          className={`w-[24.29px] h-[24.29px] sm:w-[20px] sm:h-[20px] lg:w-[24.29px] lg:h-[24.29px] ${
+            isFilterBoxOpen ? "sm:text-black lg:text-black" : "sm:text-white lg:text-white"
+          }`}
         />
-        <div className="text-xl font-medium font-['Inter']">Filter</div>
+        <div
+          className={`text-xl font-medium sm:text-base lg:text-xl ${
+            isFilterBoxOpen ? "sm:text-black lg:text-black" : "sm:text-white lg:text-white"
+          }`}
+        >
+          Filter
+        </div>
       </div>
 
       {/* Animate visibility of the FilterBox */}
       <div
         className={`mt-[37px] absolute top-full left-0 transition-all duration-500 ease-in-out transform ${
-          isFilterBoxOpen ? 'translate-y-0 opacity-100' : '-translate-y-8 opacity-0'
-        }`}
-        style={{ visibility: isFilterBoxOpen ? 'visible' : 'hidden' }}
+          isFilterBoxOpen
+            ? "translate-y-0 opacity-100"
+            : "-translate-y-8 opacity-0"
+        } sm:mt-[20px] lg:mt-[37px] lg:border lg:border-white rounded-[10px]`}
+        style={{ visibility: isFilterBoxOpen ? "visible" : "hidden" }}
       >
-        <div className="w-[282px] px-4 pt-7 pb-[69px] bg-[#e8e8e8]/20 rounded-[10px] border border-white backdrop-blur-[24.90px] flex-col justify-start items-start gap-[15px] inline-flex">
-          <div className="text-white text-[32px] font-semibold font-['Inter']">
+        <div
+          className={`w-[343px] sm:w-[500px] px-4 sm:px-6 pt-7 pb-[304px] rounded-[10px] ${
+            isFilterBoxOpen
+              ? "sm:bg-white sm:text-black sm:border-none lg:bg-[#e8e8e8]/20 lg:text-white"
+              : "sm:bg-[#e8e8e8]/20 sm:text-white lg:bg-[#e8e8e8]/20 lg:text-white"
+          } backdrop-blur-[24.90px] flex-col justify-start items-start gap-[15px] inline-flex lg:w-[282px] lg:px-4 lg:pt-7 lg:pb-[69px] rounded-[10px]`}
+        >
+          <div
+            className={`text-[32px] font-semibold sm:text-[24px] lg:text-[32px] ${
+              isFilterBoxOpen
+                ? "sm:text-black lg:text-white"
+                : "sm:text-white lg:text-white"
+            }`}
+          >
             Filter
           </div>
-          <hr className="border-t border-white w-full" />
-          <div className="text-white text-[19px] font-semibold font-['Inter'] leading-7 tracking-tight">
+          <hr className="w-full h-[2px] sm:bg-black lg:bg-white bg-black border-0" />
+
+          <div
+            className={`text-[19px] font-semibold leading-7 tracking-tight sm:text-[16px] lg:text-[19px] ${
+              isFilterBoxOpen
+                ? "sm:text-black lg:text-white"
+                : "sm:text-white lg:text-white"
+            }`}
+          >
             Resource Format
           </div>
-          <div className="flex-col justify-start items-start gap-[7px] flex">
+          <div className="flex-col justify-start items-start gap-[7px] sm:gap-[5px] lg:gap-[7px] flex">
             {Object.keys(checkedKinds).map((kind) => (
               <div
-                className="justify-start items-center gap-8 inline-flex"
+                className="justify-start items-center gap-8 sm:gap-4 lg:gap-8 inline-flex"
                 key={kind}
               >
-                <div className="justify-start items-start gap-2.5 flex">
+                <div className="justify-start items-start gap-2.5 sm:gap-2.5 lg:gap-2.5 flex">
                   <input
                     type="checkbox"
                     value={kind}
                     checked={checkedKinds[kind]}
                     onChange={handleKindCheckboxChange}
-                    className="custom-checkbox"
+                    className={`custom-checkbox ${
+                      isFilterBoxOpen ? "mobile-open" : ""
+                    }`}
                   />
                 </div>
-                <div className="text-white text-base font-medium font-['Urbanist'] leading-normal tracking-tight">
+                <div
+                  className={`text-base sm:text-sm lg:text-base font-medium ${
+                    isFilterBoxOpen
+                      ? "sm:text-black lg:text-white"
+                      : "sm:text-white lg:text-white"
+                  }`}
+                >
                   {kind}
                 </div>
               </div>
--- src/pages/AttachmentsAndEmotions.jsx
+++ src/pages/AttachmentsAndEmotions.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GeneralFilter from "../components/GeneralFilter";
 import Directory from "../components/Directory";
 import SearchBarWithCommit from "../components/SearchBarWithCommit";
@@ -7,7 +7,20 @@ function AttachmentsAndEmotions() {
   const [filteredData, setFilteredData] = useState([]);
   const [isFilterBoxOpen, setIsFilterBoxOpen] = useState(false);
   const [isInitial, setIsInitial] = useState(true); // Initial state to control first interaction
+  const [isMobile, setIsMobile] = useState(false); // State to track if it's a mobile screen
   const category = "Attachment & Emotions"; // Category specific to this page
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)"); // Mobile breakpoint at 768px
+    setIsMobile(mediaQuery.matches);
+
+    const handleResize = () => setIsMobile(mediaQuery.matches);
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleResize);
+    };
+  }, []);
 
   const handleFilterBoxToggle = () => {
     if (isInitial) {
@@ -18,19 +31,20 @@ function AttachmentsAndEmotions() {
 
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-black to-black">
-      <div className="w-full h-[50vh] md:h-[847px] bg-attachment-page md:bg-center bg-[length:auto_54.5%] bg-no-repeat md:bg-cover flex flex-col items-center justify-center">
-        <h1 className="text-center text-white text-[34px] md:text-[71px] font-extrabold font-[playfair] -mt-[161.5px]">
+      <div className="w-full h-[50vh] md:h-[847px] bg-attachment-page md:bg-center bg-[length:auto_67%] bg-no-repeat md:bg-cover flex flex-col items-center justify-center">
+        <h1 className="text-center text-white text-[34px] md:text-[71px] font-extrabold font-[playfair] -mt-[35px] md:-mt-[161.5px]">
           Attachment & Emotions
         </h1>
         <div className="flex flex-col items-center">
           <SearchBarWithCommit
             setFilteredData={setFilteredData}
-            category={category}
+            category={category}åå
           />
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-start flex-grow pb-[100px] -mt-[192px]">
+      {/* Adjusting the margin for mobile view */}
+      <div className={`flex flex-col items-center justify-start flex-grow pb-[100px] ${isMobile ? "mt-[50px]" : "-mt-[192px]"}`}>
         <div className="w-full max-w-[1200px] min-h-[500px] px-4 md:px-6 lg:px-8">
           <GeneralFilter
             currentPage={category}
@@ -39,13 +53,14 @@ function AttachmentsAndEmotions() {
             isFilterBoxOpen={isFilterBoxOpen}
             className={`transition-all duration-500 ease-in-out ${
               !isInitial &&
+              !isMobile && // Disable animation on mobile
               (isFilterBoxOpen ? "animate-slideDown" : "animate-slideUp")
             }`}
           />
-          <div className="flex flex-col pt-[37px]">
+          <div className="flex flex-col pt-[15px]">
             <div
               className={`transition-all duration-500 ease-in-out flex origin-left ${
-                isFilterBoxOpen ? "ml-[300px] shrink" : "ml-0 grow"
+                isFilterBoxOpen && !isMobile ? "ml-[300px] shrink" : "ml-0 grow"
               }`}
             >
               <Directory
