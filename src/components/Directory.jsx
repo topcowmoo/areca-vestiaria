e85c6b8c2192a597f0636dab20d03e10755ff0c0
Salvatore <mammoliti.sam@gmail.com>
2024-08-20 16:31:27 -0400

    responsiveness amendments

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 
 function Directory({ filteredData }) {
-  const rowHeight = 60; // Approximate height of each row in pixels
+  const rowHeight = 70; // Approximate height of each row in pixels
   const maxVisibleRows = 9; // Number of rows to display before scrolling
   const maxHeight = rowHeight * maxVisibleRows; // Calculate max height for 9 rows
-
+  
   return (
     <div
       className="w-full sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] p-[20px] sm:p-[20px] md:p-[25px] lg:p-[35px] xl:p-[35px] bg-white rounded-[10px] shadow-inner flex mx-auto ml-5 mr-5"
@@ -25,13 +25,13 @@ function Directory({ filteredData }) {
               href={item.link}
               target="_blank"
               rel="noopener noreferrer"
-              className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-[10px] sm:text-[10px] md:text-lg lg:text-xl font-semibold font-['Inter'] text-left hover:underline sm:px-[22px] sm:py-[11px] md:px-[25px] md:py-[12px] lg:px-[30px] lg:py-[15px] xl:px-[35px] xl:py-[18px]"
+              className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-base sm:text-base md:text-lg lg:text-xl font-semibold font-['Inter'] text-left hover:underline sm:px-[22px] sm:py-[11px] md:px-[25px] md:py-[12px] lg:px-[30px] lg:py-[15px] xl:px-[35px] xl:py-[18px]"
             >
               {item.title}
             </a>
           ))
         ) : (
-          <div className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-[10px] sm:text-[10px] md:text-lg lg:text-xl font-semibold font-['Inter'] text-left sm:px-[22px] sm:py-[11px] md:px-[25px] md:py-[12px] lg:px-[30px] lg:py-[15px] xl:px-[35px] xl:py-[18px]">
+          <div className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-base sm:text-base md:text-lg lg:text-xl font-semibold font-['Inter'] text-left sm:px-[22px] sm:py-[11px] md:px-[25px] md:py-[12px] lg:px-[30px] lg:py-[15px] xl:px-[35px] xl:py-[18px]">
             No results found
           </div>
         )}
