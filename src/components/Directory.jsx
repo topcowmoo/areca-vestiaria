0572f1ba5fada9a799b0f156f4f80e2526427673
Salvatore <mammoliti.sam@gmail.com>
2024-08-28 23:06:56 -0400

    changed text size on small screens and edited shadow

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -7,7 +7,7 @@ function Directory({ filteredData }) {
   
   return (
     <div
-      className="w-full sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] p-[20px] sm:p-[20px] md:p-[25px] lg:p-[35px] xl:p-[35px] bg-white rounded-[10px] shadow-inner flex mx-auto"
+      className="w-full sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] p-[20px] sm:p-[20px] md:p-[25px] lg:p-[35px] xl:p-[35px] bg-white rounded-[10px] shadow-custom-inner flex mx-auto"
       style={{
         height: filteredData.length > 0 ? `${maxHeight}px` : "auto",
       }}
@@ -25,13 +25,13 @@ function Directory({ filteredData }) {
               href={item.link}
               target="_blank"
               rel="noopener noreferrer"
-              className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-base sm:text-base md:text-lg lg:text-xl font-semibold font-['Inter'] text-left hover:underline sm:px-[22px] sm:py-[11px] md:px-[25px] md:py-[12px] lg:px-[30px] lg:py-[15px] xl:px-[35px] xl:py-[18px]"
+              className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-[12px] sm:text-base md:text-lg lg:text-lg font-semibold font-['Inter'] text-left hover:underline sm:px-[22px] sm:py-[11px] md:px-[25px] md:py-[12px] lg:px-[30px] lg:py-[15px] xl:px-[35px] xl:py-[18px]"
             >
               {item.title}
             </a>
           ))
         ) : (
-          <div className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-base sm:text-base md:text-lg lg:text-xl font-semibold font-['Inter'] text-left sm:px-[22px] sm:py-[11px] md:px-[25px] md:py-[12px] lg:px-[30px] lg:py-[15px] xl:px-[35px] xl:py-[18px]">
+          <div className="block px-[20px] py-[10px] bg-white rounded-[10px] shadow-custom-light text-[#1f1f1f] text-[12px] sm:text-base md:text-lg lg:text-lg font-semibold font-['Inter'] text-left">
             No results found
           </div>
         )}
