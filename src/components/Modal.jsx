7c569040d2dc8122e65e444517057e06efa37160
Salvatore <mammoliti.sam@gmail.com>
2025-03-25 21:14:01 -0400

    refactor Modal component for improved layout, responsiveness, and accessibility

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,22 +5,45 @@ const Modal = ({ isOpen, onClose, title, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center">
-      <div className="relative w-full max-w-[1100px] bg-white/80 bg-opacity-50 rounded-[18px] p-8 shadow-lg">
+    <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center z-50 overflow-y-auto landscape:items-start landscape:pt-8">
+    <div
+  className="
+    relative w-[90%] sm:w-[85%] md:w-[80%] lg:w-[75%] xl:w-[70%]
+    max-w-[1100px] bg-white/80 rounded-[18px] mt-14 px-3 sm:px-6 md:px-8 pb-8 shadow-lg
+    pt-[clamp(40px,10vh,70px)]
+    my-8 md:my-12 landscape:my-4 md:landscape:my-12
+    landscape:max-h-[90vh] md:landscape:max-h-[none] overflow-y-auto "
+>
+
         <IoCloseSharp
           className="absolute top-4 right-4 cursor-pointer w-8 h-8 text-black"
           onClick={onClose}
         />
-        <h2 className="text-center text-black text-[40px] font-extrabold font-playfair mb-6">
+       <h2
+          className="text-center text-black font-extrabold font-playfair mb-6"
+          style={{
+            fontSize: "clamp(12px, 4vw, 40px)",
+          }}
+        >
           {title}
         </h2>
-        <div className="bg-white/80 rounded-[18px] p-8 overflow-y-auto shadow-inner max-h-[400px] custom-scrollbar">
-          <div className="text-[#1e1e1e] text-[18px] font-normal font-inter leading-[37px]">
-            {children}
-          </div>
-        </div>
-      </div>
+        <div
+  className="
+    bg-white
+    rounded-[10px]
+    shadow-custom-inner
+    overflow-y-auto
+    max-h-[60vh] sm:max-h-[65vh] md:max-h-[70vh]
+    custom-scrollbar
+     pt-[10px] pb-[10px] pr-[15px] pl-[15px]
+  "
+>
+    <div className="text-[#1e1e1e] text-sm sm:text-base md:text-[18px] font-normal font-inter leading-[1.8]">
+      {children}
     </div>
+  </div>
+</div>
+      </div>
   );
 };
 
