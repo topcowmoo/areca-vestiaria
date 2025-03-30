e83eb42bc3d79440c391a2867f2f55503fb48b5d
smbanati-IZxCKm <smbanati@gmail.com>
2025-03-29 22:20:26 -0400

    added searchbar glow

--- src/components/SearchBarWithCommit.jsx
+++ src/components/SearchBarWithCommit.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { IoMdSearch } from "react-icons/io";
-import { collectionData } from "../data/collectionData";
 
-const SearchBarWithCommit = ({ category, onSearchChange }) => {
+
+const SearchBarWithCommit = ({ onSearchChange }) => {
   const [query, setQuery] = useState("");
-  const [setSearchResults] = useState([]);
-  const [setIsFocused] = useState(false);
+ 
+  
 
   const handleInputChange = (e) => {
     const value = e.target.value;
@@ -15,30 +15,16 @@ const SearchBarWithCommit = ({ category, onSearchChange }) => {
     //  Tell parent to update Directory
     onSearchChange(value);
 
-    // Local dropdown preview logic
-    if (value.trim() === "" || value.length < 3) {
-      setSearchResults([]);
-    } else {
-      const results = collectionData.filter(
-        (item) =>
-          item.category === category &&
-          item.title.toLowerCase().includes(value.toLowerCase())
-      );
-      setSearchResults(results.slice(0, 3));
-    }
+   
   };
-
-
-
-
 
   return (
     <div className="relative flex flex-col items-center justify-center p-4">
       <div
         className="
         w-[247px] h-[33px] pl-3.5 pr-[9.43px] py-[2.57px] 
-        bg-white rounded-[13.71px] shadow-[0_0_15px_5px_rgba(255,255,255,0.5)] flex items-center 
-        border border-gray-200 focus-within:shadow-[0_0_15px_5px_rgba(255,255,255,0.4)] 
+        bg-white rounded-[13.71px] shadow-[0_0_15px_5px_rgba(255,255,255,0.2)] flex items-center 
+        border border-gray-200 focus-within:shadow-[0_0_15px_5px_rgba(255,255,255,0.7)] transition-shadow duration-300 ease-in-out 
         sm:w-[247px] sm:h-[33px] sm:pl-3.5 sm:pr-[9.43px] sm:py-[2.57px] 
         md:w-[482px] md:h-[35px] md:pl-[22px] md:pr-[22px] md:py-1.5 md:rounded-[32px] 
         lg:w-[482px] lg:h-[35px] lg:pl-[22px] lg:pr-[22px] lg:py-1.5 lg:rounded-[32px]
@@ -55,8 +41,7 @@ const SearchBarWithCommit = ({ category, onSearchChange }) => {
           value={query}
           onChange={handleInputChange}
           
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setTimeout(() => setIsFocused(false), 150)}
+        
         />
         <IoMdSearch className="w-[9.86px] h-[9.86px] sm:w-[9.86px] sm:h-[9.86px] md:w-[23px] md:h-[23px] lg:w-[23px] lg:h-[23px] ml-2 text-[#9b9b9b]" />
       </div>
