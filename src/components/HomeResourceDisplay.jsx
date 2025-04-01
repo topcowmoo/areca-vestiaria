101de2968fd074b81549ff47d3ba6ac3073cec26
Salvatore <mammoliti.sam@gmail.com>
2025-04-01 12:49:08 -0400

    adjusted margin top and bottom and removed view all resources button

--- src/components/HomeResourceDisplay.jsx
+++ src/components/HomeResourceDisplay.jsx
@@ -17,7 +17,8 @@ const orderedCategories = [
 
 const HomeResourceDisplay = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6 xl:px-14">
+    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-[18px] md:px-[1px]">
+
     {orderedCategories.map((category, index) => (
       <ResourceCard key={index} category={category} />
     ))}
